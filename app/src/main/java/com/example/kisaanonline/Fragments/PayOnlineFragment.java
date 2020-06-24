5585d0ce513647e7a5a888537b952243112696b8
Sagar Nath <nathsagar24@gmail.com>
2020-06-24 13:29:16 +0530

    payment system implemented and working

--- app/src/main/java/com/example/kisaanonline/Fragments/PayOnlineFragment.java
+++ app/src/main/java/com/example/kisaanonline/Fragments/PayOnlineFragment.java
@@ -1,12 +1,14 @@
 package com.example.kisaanonline.Fragments;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -49,7 +51,8 @@ public class PayOnlineFragment extends Fragment {
 
         Map<String, String> paramMap = new HashMap<String, String>();
         paramMap.put("MID", "qmrsQK63338525663017");
-        paramMap.put("ORDER_ID", "20200620183925959000");
+        //Can't reuse same order id
+        paramMap.put("ORDER_ID", "20200821094055662000");
         paramMap.put("CUST_ID", "4e59566a67565852344e4b61623741535132596257773d3d");
         paramMap.put("MOBILE_NO", "7622017002");
         paramMap.put("EMAIL", "info@kisaanonline.net");
@@ -57,38 +60,59 @@ public class PayOnlineFragment extends Fragment {
         paramMap.put("TXN_AMOUNT", "9400.0");
         paramMap.put("WEBSITE", "DEFAULT");
         paramMap.put("INDUSTRY_TYPE_ID", "Retail");
-        paramMap.put("CALLBACK_URL", "https://bazaar.kisaanonline.net/payment-response");
-        paramMap.put("CHECKSUMHASH", "TKjOAncns2trqPsNAdYd8gcRVXgXkITcjL4H1ftYfCvVIV+9fmB8Pflr3nk0S8tRwhe0b7tgKzp5qDzlZT0jE7JODFCw6E7Jd06KD0gNErk=");
+        paramMap.put("CALLBACK_URL", "http://bazaar.kisaanonline.net/payment-response");
+        paramMap.put("CHECKSUMHASH", "FT6p1XYaAq+DYGdfMWrz6/4AAiwF1F3Hwn7wi09Hr+udNmdJxQpamD0U02M3e9QxDDB+Tm0iuAHdA20u9hUQPnCZ8W/oe03bc2GMvrnxFDU=");
         PaytmOrder Order = new PaytmOrder((HashMap<String, String>) paramMap);
 
         paytmService.initialize(Order, null);
 
         paytmService.startPaymentTransaction(getActivity(), true, true, new PaytmPaymentTransactionCallback() {
-            /*Call Backs*/
+
             public void someUIErrorOccurred(String inErrorMessage) {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
 
             public void onTransactionResponse(Bundle inResponse) {
-                Toast.makeText(getActivity().getApplicationContext(), "Payment Transaction response " + inResponse.toString(), Toast.LENGTH_LONG).show();
-                Log.v("RESPONSE : ", "" + inResponse.toString());
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
 
             public void networkNotAvailable() {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
 
             public void clientAuthenticationFailed(String inErrorMessage) {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
 
             public void onErrorLoadingWebPage(int iniErrorCode, String inErrorMessage, String inFailingUrl) {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
 
             public void onBackPressedCancelTransaction() {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
 
             public void onTransactionCancel(String inErrorMessage, Bundle inResponse) {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
         });
         return v;
     }
+
+
 
 }
